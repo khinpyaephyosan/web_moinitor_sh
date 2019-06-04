c50d71f7a68ac85199155f160190e69df31c1bc2
Lillian Phyo <khinpyaephyosan@gmail.com>
2019-06-04 15:29:07 +0630

    Update web_monitor.sh

--- web_monitor.sh
+++ web_monitor.sh
@@ -2,9 +2,10 @@
 stat=$(curl -s -o /dev/null -I -w "%{http_code}" https://www.kbzgateway.com/)
 echo $stat
 DATE=$(date '+%Y-%m-%d %H:%M:%S')
+mail_addr= ""
 if [ $stat == 200 ] 
 then
    echo "ok"
 else
-   $(mail -s "KBZGW website status alert" it.kbzgw@kbzgateway.com  <<< "KBZGW webiste was down at $DATE! Error Status Code was $stat.")
+   $(mail -s "KBZGW website status alert" $mail_addr  <<< "KBZGW webiste was down at $DATE! Error Status Code was $stat.")
 fi
